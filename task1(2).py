0dc9adbe8c37f028d3355b448a436bebe77b77bd
Md Golam Tawhid Fahad <107804579+Golam-Tawhid@users.noreply.github.com>
2023-06-13 14:57:20 +0000

    Task added

---
+++ task1(2).py
@@ -0,0 +1,28 @@
+inp = open('input1.txt','r')
+output_file = open('output1.txt','w')
+
+n,num=map(int, inp.readline().strip().split())
+
+n_list=list(map(int, inp.readline().strip().split()))
+print(n_list)
+
+dict= {}
+for i in range(n):
+    if(dict.get(n_list[i])):
+        dict[n_list[i]].append(i)
+    else:
+        dict[n_list[i]] = [i]
+    # dict.append([n_list[i],i+1])
+print(dict)
+n_list.sort()
+
+l,r= 0, n-1
+
+while l<r:
+    if n_list[l]+n_list[r]==num:
+        print(dict[n_list[l]]+1,dict[n_list[r]]+1, file=output_file)
+        break
+    elif n_list[l]+n_list[r]>num:
+        r-=1
+    else:
+        l+=1
---
+++ task1.py
@@ -0,0 +1,17 @@
+inp= open('input1.txt','r')
+output_file= open('output1.txt','w')
+
+n,num=map(int, inp.readline().strip().split())
+
+n_list=list(map(int, inp.readline().strip().split()))
+d=0
+for i in range(n):
+    for j in range(i,n-1):
+        if n_list[i]+n_list[j]==num and i!=j:
+            print(i+1,j+1, file=output_file)
+            d+=1
+            break
+if d==0:
+    print("IMPOSSIBLE", file=output_file)
+
+output_file.close()
