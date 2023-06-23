137359007a1597bf93119ad0de2fc5ebca6770d4
Golam Tawhid <tawhidfahad199@gmail.com>
2023-06-23 09:15:25 +0600

    Lab 2 task 1(1,2)added

--- Lab 2/Task 1/task1(2).py
+++ Lab 2/Task 1/task1(2).py
@@ -1,28 +1,22 @@
 inp = open('Lab 2/Task 1/input1.txt','r')
-output_file = open('Lab 2/Task 1/input1.txt','w')
+output_file = open('Lab 2/Task 1/output1.txt','w')
 
 n,num=map(int, inp.readline().strip().split())
 
 n_list=list(map(int, inp.readline().strip().split()))
-print(n_list)
 
 dict= {}
-for i in range(n):
-    if(dict.get(n_list[i])):
-        dict[n_list[i]].append(i)
-    else:
-        dict[n_list[i]] = [i]
-    # dict.append([n_list[i],i+1])
-print(dict)
-n_list.sort()
 
-l,r= 0, n-1
+for i,val in enumerate(n_list):
+    a= num - val
+    if a in dict:
+        b= dict[a]
+        print(b+1, i+1, file= output_file)
+        output_file.close()
+        inp.close()
+        exit(0)
 
-while l<r:
-    if n_list[l]+n_list[r]==num:
-        print(dict[n_list[l]]+1,dict[n_list[r]]+1, file=output_file)
-        break
-    elif n_list[l]+n_list[r]>num:
-        r-=1
-    else:
-        l+=1
+    dict[val]=i
+    
+print('IMPOSSIBLE', file=output_file)
+output_file.close()
