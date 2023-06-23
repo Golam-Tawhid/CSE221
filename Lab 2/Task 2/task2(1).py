5687d82c7f784f33c271060996c6df21484cf192
Golam Tawhid <tawhidfahad199@gmail.com>
2023-06-23 12:56:59 +0600

    lab 2 task 2(1) changed

--- Lab 2/Task 2/task2(1).py
+++ Lab 2/Task 2/task2(1).py
@@ -15,8 +15,19 @@ def final_merge(n_list, m_list):
 
     return f_list
 
-inp = open('Lab 2\Task 2\input2.txt', 'r')
-out = open('Lab 2\Task 2\output2.txt', 'w')
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+    return final_merge(left, right)
+
+
+inp = open('Lab 2/Task 2/input2.txt', 'r')
+out = open('Lab 2/Task 2/output2.txt', 'w')
 
 n = int(inp.readline().strip())
 n_list = list(map(int, inp.readline().strip().split()))
@@ -24,8 +35,11 @@ n_list = list(map(int, inp.readline().strip().split()))
 m = int(inp.readline().strip())
 m_list = list(map(int, inp.readline().strip().split()))
 
-f_list = final_merge(n_list, m_list)
+n_list= n_list+m_list
+
+f_list = merge_sort(n_list)
 
 print(' '.join(str(i) for i in f_list), file=out)
 
+inp.close()
 out.close()
