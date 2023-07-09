9593f3dd24b0d7afb92155138cc6f53aac2332bd
Golam Tawhid <tawhidfahad199@gmail.com>
2023-07-09 20:53:08 +0600

    Lab 3 task 2 updated

--- Lab3/Task 2/t2.py
+++ Lab3/Task 2/t2.py
@@ -1,44 +1,19 @@
 maxVal = float('-inf')
 
-def mergeSort(arr):
-    if len(arr) <= 1:
-        return arr
+def calculation(a,b):
+    return [max(a) + max(b, key=abs)**2]
 
-    mid = len(arr) // 2
-    left = arr[:mid]
-    right = arr[mid:]
-
-    left = mergeSort(left)
-    right = mergeSort(right)
-
-    return merge(left, right)
-
-def merge(left, right):
+def findMaxVal(arr):
     global maxVal
-    merged = []
-    i = j = 0
-
-    while i < len(left) and j < len(right):
-        if left[i] <= right[j]:
-            val = left[i] + (right[j] ** 2)
-            maxVal = max(maxVal, val)
-            merged.append(left[i])
-            i += 1
-        else:
-            val = right[j] + (left[i] ** 2)
-            maxVal = max(maxVal, val)
-            merged.append(right[j])
-            j += 1
-
-    while i < len(left):
-        merged.append(left[i])
-        i += 1
-
-    while j < len(right):
-        merged.append(right[j])
-        j += 1
-    
-    return merged
+    if len(arr)<=1:
+        return arr
+    else:
+        mid = len(arr)//2
+        a1 = findMaxVal(arr[:mid])
+        a2 = findMaxVal(arr[mid:])
+        # print(calculation(a1,a2),a1,a2)
+        maxVal= max(maxVal, calculation(a1,a2)[0])
+        return a1+a2
 
 inp = open('Lab3/Task 2/input2.txt','r')
 out = open('Lab3/Task 2/output2.txt','w')
@@ -46,7 +21,5 @@ out = open('Lab3/Task 2/output2.txt','w')
 n = int(inp.readline())
 arr = list(map(int, inp.readline().strip().split()))
 
-maxS = mergeSort(arr)
-
-print(maxS)
-print(maxVal)
+result = findMaxVal(arr)
+print(maxVal, file=out)
