e550cd1aa341842b46c48eeffa7b1cc168c0c2d3
Md Golam Tawhid Fahad <107804579+Golam-Tawhid@users.noreply.github.com>
2023-08-13 06:24:24 +0000

    lab7 task 3

---
+++ Lab 7/task3.py
@@ -0,0 +1,24 @@
+def addedge(parent, size, u, v):
+    if fparent(parent, u) != fparent(parent, v):
+        size[fparent(parent, u)] += size[fparent(parent, v)]
+    
+    parent[fparent(parent, v)] = parent[fparent(parent, u)]
+
+def fparent(parent, u):
+    if u == parent[u]:
+        return u
+    parent[u] = fparent(parent, parent[u])
+    return parent[u]
+
+
+inp = open('Lab 7/input1.txt','r')
+out = open('Lab 7/output1.txt','w')
+
+n, k = map(int, inp.readline().strip().split())
+parent = [i for i in range(n+1)]
+size= [1]*(n+1)
+
+for i in range(k):
+    u,v= map(int, inp.readline().strip().split())
+    addedge(parent, size, u, v)
+    print(size[fparent(parent, u)])
