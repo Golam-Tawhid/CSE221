2e86c58beeae7273db124b6436ef7cbe199e3e20
Md Golam Tawhid Fahad <107804579+Golam-Tawhid@users.noreply.github.com>
2023-08-13 06:46:35 +0000

    lab 8 task 1

---
+++ Lab 8/task1.py
@@ -0,0 +1,30 @@
+import heapq
+
+cost=0
+
+def addedge(parent, u, v, w):
+    global cost
+    if fparent(parent, u) != fparent(parent, v):
+        cost+=w
+    parent[fparent(parent, v)] = parent[fparent(parent, u)]
+
+def fparent(parent, u):
+    if u == parent[u]:
+        return u
+    parent[u] = fparent(parent, parent[u])
+    return parent[u]
+
+inp = open('Lab 8/input1.txt','r')
+out = open('Lab 8/output1.txt','w')
+
+n, m = map(int, inp.readline().strip().split())
+parent = [i for i in range(n+1)]
+edge = []
+
+for i in range(m):
+    u,v, w= map(int, inp.readline().strip().split())
+    heapq.heappush(edge, (w, u, v))
+while edge:
+    w,u,v = heapq.heappop(edge)
+    addedge(parent, u, v, w)
+print(cost, file=out)
